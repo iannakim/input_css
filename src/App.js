a1351914dda64f858c9fa5a70b613e670d3ec4b2
iannakim <dear.hyunji@gmail.com>
2021-02-11 21:33:31 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    email: "",
+    colorchange: false
+}
+
+    //controlled form inputs
+    handleChange = (evt) => {
+      this.setState({
+          [evt.target.name] : evt.target.value
+      })
+      console.log(evt.target)
+      if (this.state.email.includes("@")){
+        console.log("HIIIIIII")
+        this.setState({
+          colorchange: true
+        })
+      }
+      else this.setState ({
+        colorchange: false
+      })
+  }
+
+  render() {
+
+    console.log(this.state.email)
+
+    let inputColor = this.state.colorchange ? "new-box" : "box"
+
+      return (
+        <div className="App">
+          <div className="form-container">
+            <div 
+            style={{
+              marginBottom: "20px",}}>
+              <input type="text" name="email" className={inputColor} id="nemail" required autocomplete="off" onChange={this.handleChange} />
+              <label for="nemail"><span>Email Address</span></label>
+            </div>
+            <div style={{
+              marginTop: "20px",}}>
+              <input type="password" name="password" rows="2" className="box" id="npassword" required autocomplete="off"></input>
+              <label for="npassword"><span>Password</span></label>
+            </div>
+
+          </div>
+        </div>
+      );
+  }
 }
 
 export default App;
